2175f83ca0fd4b81ea62c353f9586dac88b0192c
Russell Best <ellrussbest@gmail.com>
2023-06-12 02:35:36 +0200

    message

--- 6_esbuild/notebook/src/components/preview.tsx
+++ 6_esbuild/notebook/src/components/preview.tsx
@@ -34,7 +34,9 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   useEffect(() => {
     // reset the iframe, before execution of the whole button click operation
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
--- 6_esbuild/notebook/src/components/resizable.tsx
+++ 6_esbuild/notebook/src/components/resizable.tsx
@@ -1,68 +1,4 @@
-// import { useEffect, useState } from "react";
-// import "./resizable.css";
-// import { ResizableBox, ResizableBoxProps } from "react-resizable";
-
-// interface ResizableProps {
-//   direction: "horizontal" | "vertical";
-//   children: React.ReactNode;
-// }
-
-// const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-//   let resizableProps: ResizableBoxProps;
-
-//   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
-//   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
-//   const [width, setWidth] = useState(window.innerWidth * 0.75);
-
-//   useEffect(() => {
-//     let timer: any;
-
-//     const listener = () => {
-//       if (timer) {
-//         clearTimeout(timer);
-//       }
-
-//       timer = setTimeout(() => {
-//         setInnerHeight(window.innerHeight);
-//         setInnerWidth(window.innerWidth);
-//       }, 100);
-//     };
-
-//     window.addEventListener("resize", listener);
-
-//     return () => {
-//       window.removeEventListener("resize", listener);
-//     };
-//   }, []);
-
-//   if (direction === "horizontal") {
-//     resizableProps = {
-//       className: "resize-horizontal",
-//       height: Infinity,
-//       width,
-//       resizeHandles: ["e"],
-//       maxConstraints: [innerWidth * 0.75, Infinity],
-//       minConstraints: [innerWidth * 0.2, Infinity],
-//       onResizeStop: (event, data) => {
-//         setWidth(data.size.width);
-//       },
-//     };
-//   } else {
-//     resizableProps = {
-//       height: 300,
-//       width: Infinity,
-//       resizeHandles: ["s"],
-//       maxConstraints: [Infinity, innerHeight * 0.9],
-//       minConstraints: [Infinity, 50],
-//     };
-//   }
-
-//   return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
-// };
-
-// export default Resizable;
-
-import { useEffect, useState, memo } from "react";
+import { useEffect, useState } from "react";
 import "./resizable.css";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
@@ -71,7 +7,7 @@ interface ResizableProps {
   children: React.ReactNode;
 }
 
-const Resizable: React.FC<ResizableProps> = memo(({ direction, children }) => {
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
   const [innerHeight, setInnerHeight] = useState(window.innerHeight);
@@ -122,6 +58,6 @@ const Resizable: React.FC<ResizableProps> = memo(({ direction, children }) => {
   }
 
   return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
-});
+};
 
 export default Resizable;
