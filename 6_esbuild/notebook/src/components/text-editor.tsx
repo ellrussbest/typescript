0f72c270655763ff7a2040e6a12d2be52d02b5a4
Russell Best <ellrussbest@gmail.com>
2023-06-14 05:15:06 +0200

    text editor

--- 6_esbuild/notebook/src/components/text-editor.tsx
+++ 6_esbuild/notebook/src/components/text-editor.tsx
@@ -1,12 +1,41 @@
+import "./text-editor.css"
 import MDEditor from "@uiw/react-md-editor";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const TextEditor: React.FC = () => {
   const [value, setValue] = useState<any>("**Hello world!!!**");
+  const [editing, setEditing] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor value={value} onChange={setValue} />
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <MDEditor value={value} onChange={setValue} />
+    <div onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
     </div>
   );
